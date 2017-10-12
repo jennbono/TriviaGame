a6e2ad4c1949841579dffdab44a7a0b152480793
Jennifer Bono <jennb24@gmail.com>
2017-10-12 11:32:46 -0500

    images

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+//create onclick event for start button
+
+//have one question come up at a time
+//have 20 second timer running
+//if timer === 0, correct answer will display for 5 seconds, then go to next question
+//create onclick event for each answer
+//each answer has a if/else 
+//display answer for 5 seconds
+//display picture for 5 seconds
+//automatically go on to next question after 5 seconds is up
+
+//at end of questions total number of questions correct, incorrect and unanswered are displayed
+//create onclick event for start again button that goes to question 1
