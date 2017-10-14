1e3b98344b0ea7691925327bbeb0cb092fdff8f6
Jennifer Bono <jennb24@gmail.com>
2017-10-13 19:41:13 -0500

    new buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,59 +1,87 @@
-function(){
-	var questions = [{
-		question: "What was the 1988 Championship team named?",
-		choices: ["Danny and Co.", "The Miracles", "Danny and the Miracles", "Manning's Miracles"],
-		correctAnswer: 2
-	}, {
-		question: "What year did Allen Fieldhouse open?",
-		choices: ["1954", "1955", "1956", "1957"],
-		correctAnswer: 1
-	}, {
-		question: "Which head coach is the only coach with a losing record?",
-		choices: ["James Naismith", "Ted Owens", "Phog Allen", "Dick Harp"],
-		correctAnswer: 0
-	}, {
-		question: "Which nickname did Wilt Chamberlain prefer?",
-		choices: ["Wilt the Stilt", "Goliath", "Ursa Major", "Big Dipper"],
-		correctAnswer: 3
-	}, {
-		question: "What was the final score of the 2008 National Championship game?",
-		choices: ["70-65", "70-65 OT", "75-68", "75-68 OT"],
-		correctAnswer: 3
-	}, {
-		question: "In 1966, who was called for stepping on the sidelines during a would-be game winning shot versus Texas Western?",
-		choices: ["Walt Wesley", "Al Lopes", "Jo Jo White", "Ron Franz"],
-		correctAnswer: 2
-	}, {
-		question: "In the final Border War at Allen Fieldhouse, who blocked Phil Pressey's shot to send the game into overtime?",
-		choices: ["Thomas Robinson", "Jamari Taylor", "Kevin Young", "Jeff Withey"],
-		correctAnswer: 0
-	}, {
-		question: "How many consecutive regular season Big 12 titles have the Jayhawks won, tying them with UCLA?",
-		choices: ["14", "13", "12", "11",]
-		correctAnswer: 1
-	}];
+var currentQuestion = 0;
+var gameState = 0; //0=displaying question 1=displaying answer
+var timer;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unansweredQuestions = 0;
+
+var questions = [{
+	question: "What was the 1988 Championship team named?",
+	choices: ["Danny and Co.", "The Miracles", "Danny and the Miracles", "Manning's Miracles"],
+	correctAnswer: 2
+}, {
+	question: "What year did Allen Fieldhouse open?",
+	choices: ["1954", "1955", "1956", "1957"],
+	correctAnswer: 1
+}, {
+	question: "Which head coach is the only coach with a losing record?",
+	choices: ["James Naismith", "Ted Owens", "Phog Allen", "Dick Harp"],
+	correctAnswer: 0
+}, {
+	question: "Which nickname did Wilt Chamberlain prefer?",
+	choices: ["Wilt the Stilt", "Goliath", "Ursa Major", "Big Dipper"],
+	correctAnswer: 3
+}, {
+	question: "What was the final score of the 2008 National Championship game?",
+	choices: ["70-65", "70-65 OT", "75-68", "75-68 OT"],
+	correctAnswer: 3
+}, {
+	question: "In 1966, who was called for stepping on the sidelines during a would-be game winning shot versus Texas Western?",
+	choices: ["Walt Wesley", "Al Lopes", "Jo Jo White", "Ron Franz"],
+	correctAnswer: 2
+}, {
+	question: "In the final Border War at Allen Fieldhouse, who blocked Phil Pressey's shot to send the game into overtime?",
+	choices: ["Thomas Robinson", "Jamari Taylor", "Kevin Young", "Jeff Withey"],
+	correctAnswer: 0
+}, {
+	question: "How many consecutive regular season Big 12 titles have the Jayhawks won, tying them with UCLA?",
+	choices: ["14", "13", "12", "11"],
+	correctAnswer: 1
+}];
+
+var StartGame = $('<button type="button" class="btn btn-primary btn-lg btn-color start-game">Start Game</button>')
+
+$(".game").append(StartGame);
+
+//function for in question mode
+function displayQuestion (index) {
+	$(".game").empty();
+	var questionHeader = $('<h2 class="questonTitle">'+ questions[index].question +'</h2>');
+	$(".game").append(questionHeader);
+	for (var i = 0; i < questions[index].choices.length; i++) {
+		var choiceButton = $('<button type="button" class="btn btn-primary btn-lg btn-color choice" id="choice'+i+'" >'+questions[index].choices[i]+'</button>');
+		$(".game").append(choiceButton);
+	}
+}
+
+function displayAnswer (index,button) {
+	$(".game").empty();
+	var answerHeader = $('<h2></h2>');
+		if ($(button).attr("id") === "choice" + questions[index].correctAnswer) {
+			answerHeader.html("Correct!");
+		}
+		else {
+			answerHeader.html("Incorrect!");
+		}
+	$(".game").append(answerHeader)
 }
 
 
-
-
-
-
-
-
-
+$(document).on('click', ".choice" , function() {
+	displayAnswer(currentQuestion,this);
+});
 
 //create onclick event for start button
-// $(".start-game").click(function() {
-
-// })
+$(".start-game").click(function() {
+	displayQuestion(currentQuestion);
+})
 
 //have one question come up at a time
 //have 20 second timer running
-setTimeout(twentySeconds)
-	20000;
+// setTimeout(twentySeconds)
+// 	20000;
 
-function twentySeconds();
+// function twentySeconds();
 //if timer === 0, correct answer will display for 5 seconds, then go to next question
 //create onclick event for each answer
 //each answer has a if/else 
